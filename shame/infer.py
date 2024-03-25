c59e4833bb08c0df391f62a96d86c379f14b4477
woohwan <whpark70@naver.com>
2024-03-25 18:00:52 +0900

    test 중

--- shame/infer.py
+++ shame/infer.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 model = YOLO("yolov8_plant.pt")
-results = model.predict("data/V001_tom1_00_085_e_15_20220101_27_05110138_38102607.png")
-print(results)
+results = model.predict("data/V001_tom1_00_061_e_14_20220101_27_06094434_40158887.png")
+print(results[0].save('0.jpeg'))
