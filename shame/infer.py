327af1fb80e7a65d44d650b66a81f627caa68d8c
woohwan <whpark70@naver.com>
2024-03-25 17:41:29 +0900

    added infer

--- shame/infer.py
+++ shame/infer.py
@@ -1,9 +1,4 @@
 from ultralytics import YOLO
-import cv2
 model = YOLO("yolov8_plant.pt")
-results = model("./data/V001_tom1_00_061_e_14_20220101_27_06094434_40158887.png")
-plots = results[0].plot()
-cv2.imshow("plot", plots)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-plots.save
+results = model.predict("data/V001_tom1_00_085_e_15_20220101_27_05110138_38102607.png")
+print(results)
